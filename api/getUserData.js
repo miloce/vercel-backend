18f68cbb891c57d06c9e2f3a22c1cb5b6d45ec31
miloce <admin@luozhinet.com>
2025-03-24 12:32:37 +0800

    first commit

---
+++ api/getUserData.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) return;
+  return mongoose.connect('mongodb+srv://miloce:<db_password>@miloce.nod1a.mongodb.net/?retryWrites=true&w=majority&appName=miloce', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+};
+
+const userSchema = new mongoose.Schema({
+  userId: String,
+  userData: Object,
+});
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+module.exports = async (req, res) => {
+  await connectDB();
+
+  if (req.method === 'GET') {
+    const { userId } = req.query;
+    try {
+      const user = await User.findOne({ userId });
+      res.status(200).json(user ? user.userData : {});
+    } catch (error) {
+      res.status(500).send('Error fetching data');
+    }
+  } else {
+    res.status(405).send('Method Not Allowed');
+  }
+};
---
+++ api/saveUserData.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) return;
+  return mongoose.connect('mongodb+srv://miloce:<db_password>@miloce.nod1a.mongodb.net/?retryWrites=true&w=majority&appName=miloce', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+};
+
+const userSchema = new mongoose.Schema({
+  userId: String,
+  userData: Object,
+});
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+module.exports = async (req, res) => {
+  await connectDB();
+
+  if (req.method === 'POST') {
+    const { userId, userData } = req.body;
+    try {
+      await User.findOneAndUpdate({ userId }, { userData }, { upsert: true });
+      res.status(200).send('Data saved successfully');
+    } catch (error) {
+      res.status(500).send('Error saving data');
+    }
+  } else {
+    res.status(405).send('Method Not Allowed');
+  }
+};
